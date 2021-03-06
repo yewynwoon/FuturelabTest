d69d296866c67eb75d84316e215b42b64c2b3629
yewyn <yewyn159gmail.com>
2021-03-07 03:24:58 +0800

    set up models and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "main#index"
+  get 'education', to: 'pages#education'
+  get 'experience', to: 'pages#experience'
+  get 'skills', to: 'pages#skills'
 
-  get "/education", to: "main#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
