858fb620e214a11301ba048589ccfbe1e6a91288
yewyn <yewyn159gmail.com>
2021-03-07 02:17:31 +0800

    create new models

---
+++ db/migrate/20210306181642_create_educations.rb
@@ -0,0 +1,10 @@
+class CreateEducations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :educations do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
